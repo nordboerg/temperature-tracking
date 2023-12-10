1e10c3322e6c3102ba040de98580afb18c733e38
Norbert Gresku <norbert.gresku@1account.net>
2023-12-10 16:54:51 +0100

    set the chart to store a maximum number of data points and start shifting them once the limit is reached

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,6 +1,7 @@
 import Highcharts from 'highcharts';
 import { chartConfig } from './Chart.config.js';
 import { initChartTheme } from './Chart.theme.js';
+import { STORED_READING_LIMIT } from '../../constants.js';
 
 initChartTheme(Highcharts);
 
@@ -16,8 +17,9 @@ const Chart = {
     }
 
     const { temp_c, reading_date } = reading;
+    const shiftPoints = chart.series[0].data.length >= STORED_READING_LIMIT;
 
-    chart.series[0].addPoint([reading_date.getTime(), temp_c], true);
+    chart.series[0].addPoint([reading_date.getTime(), temp_c], true, shiftPoints);
   },
 };
 
