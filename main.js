9376fe76335a2b3716ba4ad7edfc43bf751d947b
Norbert Gresku <norbert.gresku@1account.net>
2023-12-10 15:22:41 +0100

    move all app folders into an 'src' directory

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 import './style.css';
-import TemperatureCard from './components/TemperatureCard.js';
-import TemperatureTable from './components/TemperatureTable.js';
-import { getCurrentTemperature } from './services/temperature.service.js';
-import { poll } from './services/utils.js';
-import { createStore } from './store/store.js';
-import { reducer } from './store/reducer.js';
-import { addReadingAction } from './store/actions.js';
+import TemperatureCard from './src/components/TemperatureCard.js';
+import TemperatureTable from './src/components/TemperatureTable.js';
+import { getCurrentTemperature } from './src/services/temperature.service.js';
+import { poll } from './src/services/utils.js';
+import { createStore } from './src/store/store.js';
+import { reducer } from './src/store/reducer.js';
+import { addReadingAction } from './src/store/actions.js';
 
 const POLL_INTERVAL = 1000;
 
