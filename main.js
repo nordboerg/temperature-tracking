ec43a1a04ab46f2285e21bf54c41547c06b38a4a
Norbert Gresku <norbert.gresku@1account.net>
2023-12-10 14:21:44 +0100

    add initial state management and pub/sub logic

--- main.js
+++ main.js
@@ -1,1 +1,48 @@
 import './style.css'
+
+const reducer = (action, state) => {
+  switch (action.type) {
+    case 'add': {
+      return [...state, action.payload];
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+
+function createStore(reducer, initialState) {
+  let state = initialState;
+  const subscribers = new Set();
+
+  function getState() {
+    return state;
+  }
+
+  function dispatch(action) {
+    state = reducer(action, state);
+
+    subscribers.forEach((callback) => {
+      callback();
+    });
+  }
+
+  function subscribe(callback) {
+    subscribers.add(callback);
+
+    return function () {
+      if (subscribers.has(callback)) {
+        subscribers.delete(callback);
+      }
+    };
+  }
+
+  return {
+    subscribe,
+    dispatch,
+    getState,
+  };
+}
+
+const { subscribe, dispatch, getState } = createStore(reducer, []);
