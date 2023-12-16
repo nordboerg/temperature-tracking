aac990de1d56d2ba850ebf92fef863d9c657fae6
Norbert Gresku <norbert.gresku@1account.net>
2023-12-16 12:37:03 +0000

    refactor select dropdown

--- src/components/TemperatureTable.js
+++ src/components/TemperatureTable.js
@@ -1,4 +1,5 @@
 const selectOptions = [10, 25, 50, 100];
+const isSelected = (option, n) => (option === n ? 'selected' : null);
 
 const TemperatureTable = {
   numberOfRecords: 10,
@@ -14,9 +15,11 @@ const TemperatureTable = {
     document.getElementById('temperature-table-container').innerHTML = `
       <select name="records" id="record-select">
         ${selectOptions.reduce((options, option) => {
-          options += `<option value="${option}" ${
-            option === this.numberOfRecords && 'selected'
-          }>${option}</option>`;
+          options += `
+            <option value="${option}" ${isSelected(option, this.numberOfRecords)}>
+              ${option}
+            </option>
+          `;
           return options;
         }, '')}
       </select>
